0355888a34e96893b3a3b8d7116e5ee90348f457
loembetyannick <loembetyannick@icloud.com>
2026-01-22 22:39:27 +0000

    maindays

--- src/config/api.js
+++ src/config/api.js
@@ -4,5 +4,5 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5048';
 export const API_ENDPOINTS = {
   EMAIL_SUBSCRIPTION: `${API_BASE_URL}/api/EmailSubscription/subscribe`,
 };
- 
+
 export default API_BASE_URL; 
